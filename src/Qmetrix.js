fe74f5ebeb104bbf6a4738969f70ce3e91dd140c
= <kaven.feng@autodesk.com>
2016-11-22 14:00:55 +0800

    using route

--- src/Qmetrix.js
+++ src/Qmetrix.js
@@ -5,34 +5,30 @@ import React, { Component } from 'react';
 import { Navigator } from 'react-native';
 
 import MyScene from './MyScene';
+import Login from './page/Login';
 
 export default class Qmetrix extends Component {
     render() {
-        return (
-            <Navigator
-                initialRoute={{ title: 'My Initial Scene', index: 0 }}
-                renderScene={(route, navigator) =>
-                  <MyScene
-                    title={route.title}
+      return (
+        <Navigator 
+          initialRoute={{id:'login'}}
+          renderScene={this.router}
+        />
+      );
+    }
 
-                    // Function to call when a new scene should be displayed
-                    onForward={ () => {
-                      const nextIndex = route.index + 1;
-                      navigator.push({
-                        title: 'Scene ' + nextIndex,
-                        index: nextIndex,
-                      });
-                    }}
-
-                    // Function to call to go back to the previous scene
-                    onBack={() => {
-                      if (route.index > 0) {
-                        navigator.pop();
-                      }
-                    }}
-                  />
-                }
-            />
-        )
+    router(route, nav){
+      switch(route.id){
+        case 'login':
+          return (
+            <Login />
+          );
+        case 'main':
+          return (
+            <MyScene />
+          );       
+        default:
+          return (<View />);
+      }
     }
 }
