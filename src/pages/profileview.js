e50dc1943ec50398c9b30785dba6a092f5c4a0bb
= <kaven.feng@autodesk.com>
2016-11-22 15:54:55 +0800

    add profile view and hook to it

---
+++ src/pages/profileview.js
@@ -0,0 +1,20 @@
+/**
+ * Created by kaven on 22/11/2016.
+ */
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
+export default class ProfileView extends Component {
+    render() {
+        return (
+            <Text>
+                This is profile page
+            </Text>
+        );
+    }
+}
+
