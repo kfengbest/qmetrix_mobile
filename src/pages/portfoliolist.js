32a9ce4241396b2730e4bd62803e6fb826bc26ff
Macking Liu <macking.liu@autodesk.com>
2016-11-24 15:38:16 +0800

    Add refresh for request portfolios list

--- src/pages/portfoliolist.js
+++ src/pages/portfoliolist.js
@@ -1,75 +1,78 @@
 /**
  * Created by kaven on 22/11/2016.
  */
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
     StyleSheet,
     Text,
     View,
     TouchableHighlight,
+    RefreshControl,
     ListView
 } from 'react-native';
-
-import PortfolioCell from './portfoliocell'
-import PortfolioApi from '../service/qm-portfolio'
+import PortfolioCell from './portfoliocell';
+import PortfolioApi from '../service/qm-portfolio';
 
 export default class PortfoliosList extends Component {
 
-  constructor(props) {
-    super(props);
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-    this.state = {
-        dataSource: ds.cloneWithRows([]),
-    };
-    //this.portfolios = [];
-    // const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-    // this.state = {
-    //   dataSource: ds.cloneWithRows(['portfolio 1', 'portfolio 2', 'portfolio 3']),
-    // };
-  }
+    constructor(props) {
+        super(props);
+        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.state = {
+            refreshing: false,
+            dataSource: ds.cloneWithRows([]),
+        };
+    }
 
-    // _onForward = () => {
-    //     this.props.navigator.push({
-    //         title: 'Scene ' + nextIndex,
-    //     });
-    // };
-
-  onCellSelected(data){
-    console.log(data);
-    this.props.nav.pop();
-    this.props.eventEmitter.emit('portfolioChanged', {portfolio: data});
-  }
-
-  _renderRow(data) {
-    // let that = this;
-    return (
-     <PortfolioCell
-        onSelected={this.onCellSelected.bind(this,data)}
-        portfolio={data}
-     />
-    );
-  }
-
-  componentWillMount () {
-    PortfolioApi.getAll().then(function (data) {
-        this.setState({
-            dataSource: this.state.dataSource.cloneWithRows(data)
+    _onRefresh() {
+        this.setState({refreshing: true});
+        PortfolioApi.getAll(true).then(function (data) {
+            this.setState({
+                dataSource: this.state.dataSource.cloneWithRows(data)
+            });
+            this.setState({refreshing: false});
+        }.bind(this)).catch(function (error) {
+            console.log("portfolios error:", error);
         });
-        // this.setState(data.portfolios);
-            // this.portfolios = data.portfolios;
-            // console.log("portfolios:", this.portfolios);
-    }.bind(this)).catch(function (error) {
-        console.log("portfolios error:", error);
-    });
-  }
+    }
 
-  render() {
-    // let that = this;
-    return (
-      <ListView
-        dataSource={this.state.dataSource}
-        renderRow={this._renderRow.bind(this)}
-      />
-    )
-  }
+    onCellSelected(data) {
+        console.log(data);
+        this.props.nav.pop();
+        this.props.eventEmitter.emit('portfolioChanged', {portfolio: data});
+    }
+
+    _renderRow(data) {
+        return (
+            <PortfolioCell
+                onSelected={this.onCellSelected.bind(this,data)}
+                portfolio={data}
+            />
+        );
+    }
+
+    componentWillMount() {
+        PortfolioApi.getAll().then(function (data) {
+            this.setState({
+                dataSource: this.state.dataSource.cloneWithRows(data)
+            });
+        }.bind(this)).catch(function (error) {
+            console.log("portfolios error:", error);
+        });
+    }
+
+    render() {
+        return (
+            <ListView
+                dataSource={this.state.dataSource}
+                renderRow={this._renderRow.bind(this)}
+                refreshControl={
+                    <RefreshControl
+                    refreshing={this.state.refreshing}
+                    onRefresh={this._onRefresh.bind(this)}
+                    />
+                }
+            />
+        )
+    }
 }
