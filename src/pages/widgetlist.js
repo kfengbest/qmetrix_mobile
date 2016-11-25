3891b6a1b3121fafab762f3cf21c6e13f7dd2188
Shijun Lv <shijun.lv@autodesk.com>
2016-11-25 15:59:03 +0800

    widget list update when portfolio change

--- src/pages/widgetlist.js
+++ src/pages/widgetlist.js
@@ -9,7 +9,7 @@ import {
     TouchableHighlight,
     ListView
 } from 'react-native';
-
+import Global from '../service/qm-global'
 import DashboardApi from '../service/qm-dashboard'
 import WidgetApi from '../service/qm-widget'
 import WidgetCell from './widgetcell'
@@ -20,6 +20,7 @@ export default class WidgetList extends Component {
         super();
         const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
         this.state = {
+            dashboard:null,
             dashboardId: "",
             rowData: [],
             dataSource: ds.cloneWithRows([]),
@@ -29,7 +30,6 @@ export default class WidgetList extends Component {
     componentDidMount () {
         this.props.eventEmitter.addListener('dashboardChanged', this.handleDashboardChanged, this);
         this.props.eventEmitter.addListener('portfolioChanged', this.handlePortfolioChanged, this);
-
     }
 
     handlePortfolioChanged (event) {
@@ -41,35 +41,43 @@ export default class WidgetList extends Component {
     handleDashboardChanged (event) {
         console.log("dashboard changed, reload widgets...", event);
 
-        this.setState({dashboardId: event.dashboard._id});
+        this.setState({dashboard: event.dashboard});
         this.reloadDashboard();
     }
 
     reloadPortfolio () {
-        // call rest api to fetch data.
-        let items = this.state.rowData;
-        items.push(this.state.dashboardId);
+        if(this.state.portfolio._id) {
+            DashboardApi.getPortfolioDashboards(this.state.portfolio._id)
+                        .then(function (data) {
+                            console.log("portfolio dashboards:", data);
+                            if(Global.isDefined(data.default)) {
+                                this.setState({dashboard:data.default})
+                            } else {
+                                this.setState({dashboard: data.dashboards[0]});
+                            }
+                            this.reloadDashboard();
+                        }.bind(this))
+                        .catch(function (error) {
+                            console.log("error get portfolio dashbaords:", error);
+                        });
 
-        this.setState({
-            rowData: items,
-            dataSource: this.state.dataSource.cloneWithRows(items)
-        });
+        }
+
     }
 
     reloadDashboard () {
-        if(this.state.dashboardId) {
-            WidgetApi.getWidgetsOfDashboard(this.state.dashboardId)
-                        .then(function (data) {
-                            console.log("dashboard widgets:", data);
-                            this.setState({
-                                rowData: data.widgets,
-                                dataSource: this.state.dataSource.cloneWithRows(data.widgets)
-                            });
-
-                        }.bind(this))
-                        .catch(function (error) {
-                            console.log("error fetch dashboard content:", error);
-                        });
+        if(this.state.dashboard) {
+            WidgetApi.getWidgetsOfDashboard(this.state.dashboard._id)
+                     .then(function (data) {
+                         console.log("dashboard widgets:", data);
+                         this.setState({
+                             rowData: data.widgets,
+                             dataSource: this.state.dataSource.cloneWithRows(data.widgets)
+                         });
+                     }.bind(this))
+                     .catch(function (error) {
+                         console.log("error fetch dashboard content:", error);
+                     });
         }
     }
 
