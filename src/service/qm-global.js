2e12c3c14ea538c2e3ad704104f97fa4de8426cc
= <kaven.feng@autodesk.com>
2018-08-31 09:15:51 +0800

    last commit

--- src/service/qm-global.js
+++ src/service/qm-global.js
@@ -3,10 +3,10 @@
  * the global namespace holding all globally shared stuff
  */
 
-// const qmBaseUrl = "https://qmetrix.autodesk.com/";
-// const qmAPIBaseUrl = "https://qmetrix.autodesk.com/api/";
-const qmBaseUrl = "http://localhost:8000/";
-const qmAPIBaseUrl = "http://localhost:8000/api/";
+ const qmBaseUrl = "https://qmetrix.autodesk.com/";
+ const qmAPIBaseUrl = "https://qmetrix.autodesk.com/api/";
+//const qmBaseUrl = "http://localhost:8000/";
+//const qmAPIBaseUrl = "http://localhost:8000/api/";
 
 
 export default class Global {
